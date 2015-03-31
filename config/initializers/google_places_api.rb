7035c9369a2c68fc2d5594bde041d54d61e4d435
Travis Eubanks <tmeubanks113@gmail.com>
2015-03-31 12:20:15 -0500

    adjust env

--- config/initializers/google_places_api.rb
+++ config/initializers/google_places_api.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  GOOGLE_PLACES_API_KEY = ENV[‘GOOGLE_PLACES_API’]
+  GOOGLE_PLACES_API_KEY = ENV[GOOGLE_PLACES_API]
 else
   GOOGLE_PLACES_API_KEY = YAML.load_file(Rails.root.join('config/application.yml'))['googles_api_key']
 end
