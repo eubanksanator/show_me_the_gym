364cb8c6c4cb83b1eda196859e4c7752b16cfc09
Travis Eubanks <tmeubanks113@gmail.com>
2015-03-31 13:09:41 -0500

    adds double quotes to con

--- config/initializers/google_places_api.rb
+++ config/initializers/google_places_api.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  GOOGLE_PLACES_API_KEY = ENV[‘ZEBRA’]
+  GOOGLE_PLACES_API_KEY = ENV["ZEBRA"]
 else
   GOOGLE_PLACES_API_KEY = YAML.load_file(Rails.root.join('config/application.yml'))['googles_api_key']
 end
